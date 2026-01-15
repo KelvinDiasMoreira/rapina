b3601c8c540800e8f835b39912cbdf4993b2e0c4
Antonio Souza <arfs.antonio@gmail.com>
2026-01-14 23:21:41 -0500

    feat: add prelude
    Signed-off-by: Antonio Souza <arfs.antonio@gmail.com>

--- rapina/src/lib.rs
+++ rapina/src/lib.rs
@@ -5,3 +5,14 @@ pub mod handler;
 pub mod response;
 pub mod router;
 pub mod server;
+
+pub mod prelude {
+    pub use crate::app::Rapina;
+    pub use crate::error::{Error, Result};
+    pub use crate::extract::Json;
+    pub use crate::response::IntoResponse;
+    pub use crate::router::Router;
+
+    pub use http::{Method, StatusCode};
+    pub use serde::{Deserialize, Serialize};
+}
