54329a5e17f3afe6af8658ebf4b1db5f26f207ad
Antonio Souza <arfs.antonio@gmail.com>
2026-02-27 15:36:58 -0500

    Update rapina-cli/src/common/urls.rs

--- rapina-cli/src/common/urls.rs
+++ rapina-cli/src/common/urls.rs
@@ -1,6 +1,6 @@
 const PATH_BASE: &str = "__rapina";
 
-pub fn build_routes_url(host: &String, port: &u16) -> String {
+pub fn build_routes_url(host: &str, port: u16) -> String {
     format!("http://{}:{}/{}/routes", host, port, PATH_BASE)
 }
 
