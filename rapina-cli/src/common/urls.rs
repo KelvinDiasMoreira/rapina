269d7a43e6be55986fd5faa41bb45ffb5203cd08
Antonio Souza <arfs.antonio@gmail.com>
2026-02-27 15:37:08 -0500

    Update rapina-cli/src/common/urls.rs

--- rapina-cli/src/common/urls.rs
+++ rapina-cli/src/common/urls.rs
@@ -4,6 +4,6 @@ pub fn build_routes_url(host: &str, port: u16) -> String {
     format!("http://{}:{}/{}/routes", host, port, PATH_BASE)
 }
 
-pub fn build_openapi_url(host: &String, port: &u16) -> String {
+pub fn build_openapi_url(host: &str, port: u16) -> String {
     format!("http://{}:{}/{}/openapi.json", host, port, PATH_BASE)
 }
